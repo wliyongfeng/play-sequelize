6c76cd4e1cf07939acfe1a3d2a2d9268096e3946
Yongfeng LI <wliyongfeng@gmail.com>
2025-01-06 23:25:04 +0800

    Init sequelize with uri

--- src/play.js
+++ src/play.js
@@ -1,9 +1,7 @@
+require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', '123456', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(process.env.POSTGRES_URI);
 
 (async () => {
   try {
